546045d2b9a0aa7b1b0adafdcdbd7b892399e126
Surya Nandeesh <snandeesh@student.neumont.edu>
2024-01-24 14:30:31 -0700

    1/24/2024 need to finish dijkstra

--- Assets/Scripts/AINavNode.cs
+++ Assets/Scripts/AINavNode.cs
@@ -21,7 +21,7 @@ public class AINavNode : MonoBehaviour
 		{
 			if (navPath.targetNode == this)
 			{
-				navPath.targetNode = GetRandomNeighbor();
+				navPath.targetNode = navPath.GetNextAINavNode(navPath.targetNode);
 			}
 		}
 	}
@@ -32,7 +32,7 @@ public class AINavNode : MonoBehaviour
 		{
 			if (navPath.targetNode == this)
 			{
-				navPath.targetNode = GetRandomNeighbor();
+				navPath.targetNode = navPath.GetNextAINavNode(navPath.targetNode);
 			}
 		}
 	}
@@ -68,5 +68,15 @@ public class AINavNode : MonoBehaviour
 		return (nodes == null) ? null : nodes[Random.Range(0, nodes.Length)];
 	}
 
-	#endregion
+    #endregion
+
+    public static void ResetNodes()
+    {
+        var nodes = GetAINavNodes();
+        foreach (var node in nodes)
+        {
+            node.Parent = null;
+            node.Cost = float.MaxValue;
+        }
+    }
 }
--- Assets/Scripts/AINavPath.cs
+++ Assets/Scripts/AINavPath.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+[RequireComponent(typeof(AINavAgent))]
 public class AINavPath : MonoBehaviour
 {
-	[SerializeField] private AINavNode startNode;
+	public enum ePathType
+	{
+		Waypoint,
+		Dijkstra,
+		AStar
+	}
+	[SerializeField] ePathType pathType;
+	[SerializeField] AINavNode startNode;
+	[SerializeField] AINavNode endNode;
+
+	AINavAgent agent;
+
+	List<AINavNode> path = new List<AINavNode>();
 
 	public AINavNode targetNode { get; set; } = null;
 	public Vector3 destination 
@@ -13,23 +26,51 @@ public class AINavPath : MonoBehaviour
 		get 
 		{ 
 			return (targetNode != null) ? targetNode.transform.position : Vector3.zero; 
-		} 
+		}
+		set
+		{
+			if (pathType == ePathType.Waypoint) { targetNode = agent.GetNearestAINavNode(value); }
+			else if(pathType == ePathType.Dijkstra || pathType == ePathType.AStar)
+			{
+				GeneratePath(startNode, endNode);
+			}
+		}
 	}
 
 	private void Start()
 	{
+		agent = GetComponent<AINavAgent>();
 		targetNode = (startNode != null) ? startNode : AINavNode.GetRandomAINavNode(); 
-		
 	}
 
-	public bool HasPath()
+	public bool HasTarget()
 	{
 		return targetNode != null;
 	}
 
 	public AINavNode GetNextAINavNode(AINavNode node)
 	{
-		return node.GetRandomNeighbor();
+		if (pathType == ePathType.Waypoint) return node.GetRandomNeighbor();
+		if (pathType == ePathType.Dijkstra || pathType == ePathType.AStar) return GetNextPathAINavNode(node);
+		return null;
+	}
+
+	private void GeneratePath(AINavNode startNode, AINavNode endNode)
+	{
+		AINavNode.ResetNodes();
+		AINavDijkstra.Generate(startNode, endNode, ref path);
+	}
+
+	private AINavNode GetNextPathAINavNode(AINavNode node)
+	{
+		if(path.Count == 0) return null;
+		int index = path.FindIndex(pathNode => pathNode == node);
+		if(index == -1) return null;
+		if(index + 1 == path.Count) return null;
+
+		AINavNode nextNode = path[index + 1];
+
+		return null;
 	}
 
 	/*
